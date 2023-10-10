44b2da94fbdbb662ae4c9deab8e0ffac49ed62c7
WesterfieldCRL <anastasicrl@gmail.com>
2023-10-10 11:12:19 -0500

    adding code

---
+++ src/main/java/Room.java
@@ -0,0 +1,88 @@
+public class Room {
+    public enum Quality { //probably want to flip order so that economy has the lowest value etc.
+        EXECUTIVE,
+        BUSINESS,
+        COMFORT,
+        ECONOMY
+    }
+    public enum BedType {
+        TWIN,
+        FULL,
+        QUEEN,
+        KING
+    }
+    private Room.Quality quality;
+    private Room.BedType bedType;
+    private int ID;
+    private boolean isSmoking;
+    private int numBeds;
+    private boolean isReserved;
+    public Room(Room.Quality quality, Room.BedType bedType, boolean isSmoking, int ID, int numBeds, boolean isReserved) {
+        this.quality = quality;
+        this.bedType = bedType;
+        this.isSmoking = isSmoking;
+        this.ID = ID;
+        this.numBeds = numBeds;
+        this.isReserved = isReserved;
+    }
+
+    public double getDailyRate() //TODO: determine method for calculating daily rate
+    {
+        return 1.0;
+    }
+    public void modifyInfo() //TODO: implement function
+    {
+
+    }
+
+
+    //Getters and setters
+    public Quality getQuality() {
+        return quality;
+    }
+
+    public void setQuality(Quality quality) {
+        this.quality = quality;
+    }
+
+    public BedType getBedType() {
+        return bedType;
+    }
+
+    public void setBedType(BedType bedType) {
+        this.bedType = bedType;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public boolean isSmoking() {
+        return isSmoking;
+    }
+
+    public void setSmoking(boolean smoking) {
+        isSmoking = smoking;
+    }
+
+    public int getNumBeds() {
+        return numBeds;
+    }
+
+    public void setNumBeds(int numBeds) {
+        this.numBeds = numBeds;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        isReserved = reserved;
+    }
+
+}
