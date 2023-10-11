53d2848573438cdf6816692e9713d52cd81507f6
Carter Collins <carter_collins3@baylor.edu>
2023-10-11 13:38:54 -0500

    updated Room and Cruise

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,10 @@ public class main {
     public static int numRooms = 40;
 
     public static void main(String[] args) {
+        Cruise c = new Cruise("Burmuda Triangle");
 
+        c.readCruiseRooms("cruise1.csv");
+
+        c.printCruise();
     }
 }
