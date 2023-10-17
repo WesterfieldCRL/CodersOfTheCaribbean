3ef54bc02ad9ab2fdf552b934cd0221dd7ca9053
AlexBarattaAltius <alexander.baratta@altius-int.com>
2023-10-17 14:34:53 -0500

    added back newline to guest

--- src/main/java/Person/Guest.java
+++ src/main/java/Person/Guest.java
@@ -45,7 +45,7 @@ public class Guest extends Person {
 
         if (conflictChecker(username, password)) {
             String data = username + "," + password + "," + "GUEST" + "," + name + "," + address + "," + email
-                    + "," + creditCardNumber + "," +creditCardExpirationDate;
+                    + "," + creditCardNumber + "," +creditCardExpirationDate + "\n";
 
             return Guest.fileWriter(data);
         }
