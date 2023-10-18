b53fd26598c7d97b966459eb95f51490c7e48098
Winecrow <143528286+Winecrow@users.noreply.github.com>
2023-10-18 10:31:49 -0500

    Create BillingInformation.java

---
+++ src/main/java/Billing/BillingInformation.java
@@ -0,0 +1,9 @@
+package Billing;
+public class BillingInformation{
+  private String holderName;
+  private String cardNumber;
+  private String expirationDate;
+  private String CCV;
+  private String postalCode;
+  
+}
